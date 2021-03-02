f8641973c8df626642567958f4139bbbf0e74396
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-03-02 21:51:17 +0530

    Update BMS_AttributeCheck.c

--- BMS_AttributeCheck.c
+++ BMS_AttributeCheck.c
@@ -1,7 +1,30 @@
+/*
+ *
+ ***************************************************************************************************
+ * Author:             Harshitha Subramani
+ * Date:               23/02/2020  
+ * Filename:           telecolor_test.h
+ *
+ ****************************************************************************************************
+*
+***************************************************************************************
+* Includes
+***************************************************************************************
+*/
 
 #include <stdio.h>
 #include <assert.h>
 #include "BMS_AttributeCheck.h"
+
+/*
+***************************************************************************************
+* Methods
+***************************************************************************************
+*
+ *This Function Checks if the Battery attribute value read is less than the defined Minimum limit
+ * \param     attribute_value             Battery reading
+ * \param     attribute                   This structure states attribute name , minimum and maximum limit
+ * \return                                returns 1 if the Battery reading is less then the defined Minimum limit else returns 0     */         
 
 int Check_Attributelow(float attribute_value,struct attribute s_attribute){
     
@@ -13,6 +36,11 @@ int Check_Attributelow(float attribute_value,struct attribute s_attribute){
     return(attribute_value < s_attribute.attribute_minvalue);
 }
 
+ /*This Function Checks if the Battery attribute value read is greater than the defined Maximum limit
+ * \param     attribute_value             Battery reading
+ * \param     attribute                   This structure states attribute name , minimum and maximum limit
+ * \return                                returns 1 if the Battery reading is greater then the defined Maximum limit else returns 0     */    
+
 int Check_AttributeHigh(float attribute_value,struct attribute s_attribute){
     if(attribute_value > s_attribute.attribute_maxvalue){
     printf("%s is high\n",s_attribute.attribute_name);
@@ -21,7 +49,9 @@ int Check_AttributeHigh(float attribute_value,struct attribute s_attribute){
     return(attribute_value > s_attribute.attribute_maxvalue);
 }
 
-
+ /*This Function Checks if the Battery is normal or abnormal based on defined Maximum and Minimum limts of each attribute
+ * \param     temperature, soc,chargeRate   Input Attributes
+  * \return                                 returns 1 if the Battery reading is normal else returns 0                           */   
 
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
@@ -39,6 +69,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
     return!(temperature_check + soc_check + chargeRate_check);
    
 }
+
 int main() {
 assert(batteryIsOk(45,70,0.7));   
 assert(!batteryIsOk(50, 70, 0.7));
