945823b2878a721d39873a933565773a16b814ed
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-03-02 21:53:08 +0530

    Update BMS_AttributeCheck.h

--- BMS_AttributeCheck.h
+++ BMS_AttributeCheck.h
@@ -1,14 +1,28 @@
-//
-//  BMS_AttributeCheck.h
-//  Assesment3
-//
-//  Created by Harshitha S on 02/03/21.
-//
+/*
+ *
+ ***************************************************************************************************
+ * Author:             Harshitha Subramani
+ * Date:               23/02/2020  
+ * Filename:           telecolor_test.h
+ *
+ ****************************************************************************************************
+*
+***************************************************************************************
+* Includes
+***************************************************************************************
+*/
 
 #ifndef BMS_AttributeCheck_h
 #define BMS_AttributeCheck_h
 
 #include <stdio.h>
+
+/*
+***************************************************************************************
+* Definitions
+***************************************************************************************
+*/
+
 struct attribute
 {
     char attribute_name[20];
@@ -21,6 +35,11 @@ struct attribute s_temperature ={"temperature",45,0};
 struct attribute s_soc = {"soc",80,20};
 struct attribute s_chargeRate = {"chargeRate",0.8,0};
 
+/*
+***************************************************************************************
+* Declarations
+***************************************************************************************
+*/
 int Check_Attributelow(float attribute_value,struct attribute s_attribute);
 int Check_AttributeHigh(float attribute_value,struct attribute s_attribute);
 int batteryIsOk(float temperature, float soc, float chargeRate);
