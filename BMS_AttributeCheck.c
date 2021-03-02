e35db953300cb41d16fea4caae0853d6fbe23b7f
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-03-02 21:31:06 +0530

    Update BMS_AttributeCheck.c

--- BMS_AttributeCheck.c
+++ BMS_AttributeCheck.c
@@ -1,9 +1,4 @@
-//
-//  BMS_AttributeCheck.c
-//  Assesment3
-//
-//  Created by Harshitha S on 02/03/21.
-//
+
 #include <stdio.h>
 #include <assert.h>
 #include "BMS_AttributeCheck.h"
@@ -14,6 +9,7 @@ int Check_Attributelow(float attribute_value,struct attribute s_attribute){
     {
         printf("%s is low\n",s_attribute.attribute_name);
     }
+    
     return(attribute_value < s_attribute.attribute_minvalue);
 }
 
@@ -21,6 +17,7 @@ int Check_AttributeHigh(float attribute_value,struct attribute s_attribute){
     if(attribute_value > s_attribute.attribute_maxvalue){
     printf("%s is high\n",s_attribute.attribute_name);
     }
+    
     return(attribute_value > s_attribute.attribute_maxvalue);
 }
 
@@ -29,18 +26,23 @@ int Check_AttributeHigh(float attribute_value,struct attribute s_attribute){
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
     int temperature_check,soc_check,chargeRate_check =0;
-    temperature_check = Check_Attributelow(temperature,s_temperature) +   Check_AttributeHigh(temperature,s_temperature);
+    
+    temperature_check = Check_Attributelow(temperature,s_temperature) + Check_AttributeHigh(temperature,s_temperature);
     soc_check = Check_Attributelow(soc,s_soc) + Check_AttributeHigh(soc,s_soc);
     chargeRate_check = Check_AttributeHigh(chargeRate,s_chargeRate);
+    
     if((temperature_check + soc_check + chargeRate_check)==0)
     {
-        printf("battery is ok");
+        printf("Battery is ok\n");
     }
+    
     return!(temperature_check + soc_check + chargeRate_check);
    
 }
 int main() {
-assert(batteryIsOk(45,70,0.7));
-   // batteryIsOk(45,70,0.7);
- assert(!batteryIsOk(50, 85, 0));
+assert(batteryIsOk(45,70,0.7));   
+assert(!batteryIsOk(50, 70, 0.7));
+assert(!batteryIsOk(45, 85, 0.7));
+assert(!batteryIsOk(45, 30, 1));
+assert(!batteryIsOk(50, 85, 0.9));
 }
